4811f64ec1f8637f1bc427964077840c538ed92e
João Lodi <joaolodi@SGBR.intra>
2026-02-19 17:16:04 -0300

    Feat: Cash register files created

---
+++ Backend/app/Domain/Financial/Cash/Controllers/CashController.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Domain\Financial\Cash\Controllers;
+
+class CashController
+{
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
---
+++ Backend/app/Domain/Financial/Cash/Services/CashService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Domain\Financial\Cash\Services;
+
+class CashService
+{
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
